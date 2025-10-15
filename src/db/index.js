b5d72549dcfee75886e1458293ef44c74aee1ec5
Jitu Yadav <jitendra.bechan.yadav@gmail.com>
2025-10-15 12:36:14 +0530

    added cicd pipeline file

--- src/db/index.js
+++ src/db/index.js
@@ -2,12 +2,12 @@
 
 const { Sequelize } = require('sequelize');
 
-const dialect = process.env.DB_DIALECT || 'mysql';
-const host = process.env.DB_HOST || 'localhost';
-const port = Number(process.env.DB_PORT || 3306);
-const database = process.env.DB_NAME || 'auth_db';
-const username = process.env.DB_USER || 'root';
-const password = process.env.DB_PASSWORD || '';
+const dialect = process.env.DB_DIALECT;
+const host = process.env.DB_HOST;
+const port = Number(process.env.DB_PORT);
+const database = process.env.DB_NAME;
+const username = process.env.DB_USER;
+const password = process.env.DB_PASSWORD;
 
 const sequelize = new Sequelize(database, username, password, {
     host,
